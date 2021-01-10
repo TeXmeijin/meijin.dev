93271dcb706dd9dec0a1d948d77e220ff6ee7a4d
TeXmeijin <i8858garden@gmail.com>
2021-01-10 21:20:20 +0900

    initial setup

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export type Member = {
+  name: string;
+  avatarSrc: string;
+  role?: string;
+  bio?: string;
+  sources?: string[];
+  includeUrlRegex?: string;
+  excludeUrlRegex?: string;
+  githubUsername?: string;
+  twitterUsername?: string;
+  websiteUrl?: string;
+};
+
+export type PostItem = {
+  authorName: string;
+  title: string;
+  link: string;
+  contentSnippet?: string;
+  isoDate?: string;
+  dateMiliSeconds: number;
+};
